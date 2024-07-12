fe9a998bfe4f875c1ea458d7ec0cfb2820521c49
ticlk <1656200743@qq.com>
2024-07-12 11:40:56 +0800

    Update Untitled Notebook 2024-07-12 11_29_22.py

--- Untitled Notebook 2024-07-12 11_29_22.py
+++ Untitled Notebook 2024-07-12 11_29_22.py
@@ -6,3 +6,10 @@
 
 # MAGIC %sql
 # MAGIC insert into table0712(id, fname) values(1,'test')
+
+# MAGIC %sql
+# MAGIC insert into table0712(id, fname) values(2,'test 2')
+
+# MAGIC %sql
+# MAGIC update table0712 set fname = 'update 2' where id =2
+
