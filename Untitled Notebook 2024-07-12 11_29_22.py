aee5cf46d1ee8304aef12c8cb6159c3b4df9ef1c
ticlk <ticlk>
2024-07-12 03:37:59 +0000

    commit 1

--- Untitled Notebook 2024-07-12 11_29_22.py
+++ Untitled Notebook 2024-07-12 11_29_22.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC create table table0712( id int, fname varchar(20))
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC insert into table0712(id, fname) values(1,'test')
